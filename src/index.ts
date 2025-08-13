0a353aaf1831d17cecd8c6ec49ea808fcdf0ccbc
doubiedev <thedoubiedev@gmail.com>
2025-08-13 12:19:02 +1000

    complete 1.4

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,53 @@ import { setUser, readConfig } from "./config.js";
 
 function main() {
     setUser("Nick");
-    const cfg = readConfig();
-    console.log(cfg);
+
+    const registry: CommandsRegistry = {};
+    registerCommand(registry, "login", handlerLogin);
+
+    let args = process.argv.slice(2);
+    if (args.length <= 0) {
+        console.log("expected at least one argument");
+        process.exit(1);
+    }
+
+    const cmdName = args[0];
+    args = args.slice(1);
+    runCommand(registry, cmdName, ...args);
 }
 
 main();
+
+type CommandHandler = (cmdName: string, ...args: string[]) => void;
+type CommandsRegistry = Record<string, CommandHandler>;
+
+function handlerLogin(cmdName: string, ...args: string[]) {
+    if (args.length <= 0) {
+        console.log("expected at least one argument (username)");
+        process.exit(1);
+    }
+
+    setUser(args[0]);
+    console.log(`user ${args[0]} has been set`);
+}
+
+function registerCommand(
+    registry: CommandsRegistry,
+    cmdName: string,
+    handler: CommandHandler,
+) {
+    registry[cmdName] = handler;
+}
+
+function runCommand(
+    registry: CommandsRegistry,
+    cmdName: string,
+    ...args: string[]
+) {
+    const handler = registry[cmdName];
+    if (!handler) {
+        console.log(`Unknown command: ${cmdName}`);
+        process.exit(1);
+    }
+    handler(cmdName, ...args);
+}
