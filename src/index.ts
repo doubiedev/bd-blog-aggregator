11107858ba38807cd557971df1f08951f1da445f
doubiedev <thedoubiedev@gmail.com>
2025-08-14 15:51:32 +1000

    complete 2.3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { setUser, readConfig } from "./config.js";
+import { createUser, getUser } from "./lib/db/queries/users.js";
 
-function main() {
-    setUser("Nick");
+type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
+type CommandsRegistry = Record<string, CommandHandler>;
 
+async function main() {
     const registry: CommandsRegistry = {};
     registerCommand(registry, "login", handlerLogin);
+    registerCommand(registry, "register", handlerRegister);
 
     let args = process.argv.slice(2);
     if (args.length <= 0) {
@@ -14,23 +17,12 @@ function main() {
 
     const cmdName = args[0];
     args = args.slice(1);
-    runCommand(registry, cmdName, ...args);
+    await runCommand(registry, cmdName, ...args);
+
+    process.exit(0);
 }
 
 main();
-
-type CommandHandler = (cmdName: string, ...args: string[]) => void;
-type CommandsRegistry = Record<string, CommandHandler>;
-
-function handlerLogin(cmdName: string, ...args: string[]) {
-    if (args.length <= 0) {
-        console.log("expected at least one argument (username)");
-        process.exit(1);
-    }
-
-    setUser(args[0]);
-    console.log(`user ${args[0]} has been set`);
-}
 
 function registerCommand(
     registry: CommandsRegistry,
@@ -40,7 +32,7 @@ function registerCommand(
     registry[cmdName] = handler;
 }
 
-function runCommand(
+async function runCommand(
     registry: CommandsRegistry,
     cmdName: string,
     ...args: string[]
@@ -50,5 +42,48 @@ function runCommand(
         console.log(`Unknown command: ${cmdName}`);
         process.exit(1);
     }
-    handler(cmdName, ...args);
+    await handler(cmdName, ...args);
+}
+
+async function handlerLogin(cmdName: string, ...args: string[]) {
+    if (args.length <= 0) {
+        console.log("expected at least one argument (username)");
+        process.exit(1);
+    }
+
+    const username = args[0];
+    const user = await getUser(username);
+    if (!user) {
+        throw new Error("user doesn't exist");
+    }
+
+    setUser(username);
+    console.log(`user ${username} has been set`);
+}
+
+async function handlerRegister(cmdName: string, ...args: string[]) {
+    if (args.length <= 0) {
+        console.log("expected at least one argument (username)");
+        process.exit(1);
+    }
+
+    const username = args[0];
+    const user = await getUser(username);
+    if (user !== undefined) {
+        throw new Error("user already exists");
+    }
+
+    try {
+        await createUser(username);
+        console.log(`user ${username} successfully created`);
+        console.log(await getUser(username));
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+
+    console.log("setting user here");
+    setUser(username);
+    console.log(readConfig());
+    process.exit(0);
 }
