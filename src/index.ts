9753d85134dbfb503fc8518ea34d2167174146b9
doubiedev <thedoubiedev@gmail.com>
2025-11-26 15:04:48 +1100

    complete and match 4.2

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ import {
     handlerLogin,
     handlerRegister,
     handlerListUsers,
-    handlerListFollowedFeeds,
 } from "./commands/users";
 import { handlerReset } from "./commands/reset";
 import { handlerAgg } from "./commands/aggregate";
 import { handlerAddFeed, handlerListFeeds } from "./commands/feeds";
 import { handlerFollow, handlerListFeedFollows } from "./commands/feed-follows";
+import { middlewareLoggedIn } from "./middleware";
 
 async function main() {
     const args = process.argv.slice(2);
@@ -31,10 +31,22 @@ async function main() {
     registerCommand(commandsRegistry, "reset", handlerReset);
     registerCommand(commandsRegistry, "users", handlerListUsers);
     registerCommand(commandsRegistry, "agg", handlerAgg);
-    registerCommand(commandsRegistry, "addfeed", handlerAddFeed);
+    registerCommand(
+        commandsRegistry,
+        "addfeed",
+        middlewareLoggedIn(handlerAddFeed),
+    );
     registerCommand(commandsRegistry, "feeds", handlerListFeeds);
-    registerCommand(commandsRegistry, "follow", handlerFollow);
-    registerCommand(commandsRegistry, "following", handlerListFeedFollows);
+    registerCommand(
+        commandsRegistry,
+        "follow",
+        middlewareLoggedIn(handlerFollow),
+    );
+    registerCommand(
+        commandsRegistry,
+        "following",
+        middlewareLoggedIn(handlerListFeedFollows),
+    );
 
     try {
         await runCommand(commandsRegistry, cmdName, ...cmdArgs);
@@ -48,6 +60,5 @@ async function main() {
     }
     process.exit(0);
 }
-import { handlerFollow } from "./commands/feed-follows";
 
 main();
